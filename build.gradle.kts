7468d732f6484aa887af660ca62687f17d901ad8
Dennis van der Zalm <dennis.vanderzalm@tmpro.eu>
2021-07-28 15:49:46 +0200

    Upgrade AGP to 7.1.0-alpha05

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${ProjectConfig.kotlinVersion}")
-        classpath("com.android.tools.build:gradle:7.1.0-alpha02")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha05")
         classpath(Dependencies.SqlDelight.gradle_plugin)
     }
 }
