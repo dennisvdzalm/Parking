88956b3bff638fa7b8b0eb44d645d3e798534e3c
Dennis van der Zalm <info@dennis.technology>
2023-01-29 00:20:52 +0100

    Add create parking reservation feature and possibility to end a reservation

--- parking-ios/parking/parking/views/history/HistoryViewModel.swift
+++ parking-ios/parking/parking/views/history/HistoryViewModel.swift
@@ -9,18 +9,31 @@ import shared
 @MainActor
 class HistoryViewModel: ObservableObject {
     @Published var history = [ParkingHistoryItem]()
-    
+
     private let getParkingHistoryUseCase: GetParkingHistoryUseCase
-    
-    init(getParkingHistoryUseCase: GetParkingHistoryUseCase) {
+    private let endParkingHistoryUseCase: EndParkingReservationUseCase
+
+    init(
+            getParkingHistoryUseCase: GetParkingHistoryUseCase,
+            endParkingHistoryUseCase: EndParkingReservationUseCase
+    ) {
         self.getParkingHistoryUseCase = getParkingHistoryUseCase
+        self.endParkingHistoryUseCase = endParkingHistoryUseCase
     }
-    
+
     func getParkingHistory() async {
         do {
             history = try await getParkingHistoryUseCase.invoke()
         } catch {
             print("Failed with error: \(error)")
+        }
+    }
+
+    func endParkingReservation(reservationId: Int32) async {
+        do {
+            try await endParkingHistoryUseCase.invoke(reservationId: reservationId)
+        } catch {
+            print("Failed to stop transaction \(error)")
         }
     }
 }
--- parking-ios/parking/parking/views/login/LoginViewModel.swift
+++ parking-ios/parking/parking/views/login/LoginViewModel.swift
@@ -8,24 +8,22 @@
 import Foundation
 import shared
 
+@MainActor
 class LoginViewModel: ObservableObject {
-    @Published var email = ""
+    @Published var passcode = ""
     @Published var password = ""
 
     private let loginUseCase: LoginUseCase
-    
-    init(loginUseCase: LoginUseCase){
+
+    init(loginUseCase: LoginUseCase) {
         self.loginUseCase = loginUseCase
     }
-    
-    func login() {
-    
-        Task { @MainActor in
-            do {
-                try await loginUseCase.invoke(username: self.email, password: self.password)
-            } catch {
-                print("Error logging in: \(error)")
-            }
-                }
+
+    func login() async {
+        do {
+            try await loginUseCase.invoke(username: self.passcode, password: self.password)
+        } catch {
+            print("Error logging in: \(error)")
         }
+    }
 }
